c48da82512646864e9cdd218854e7f26035a68ec
Kelly online <kellyonline2016@gmail.com>
2020-03-31 00:49:17 +0100

    added a get method and callback

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const express = require("express");
 
 const app = express();
 
-app.listen(8000);
+app.get('/', (req, res) => { res.send("Connected to server")})
+
+app.listen(8000, () => { console.log("Server is running on port " + port)});
