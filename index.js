2f02ef77641836c180a48c18a908429429b2ecdf
Kelly online <kellyonline2016@gmail.com>
2020-03-31 01:12:17 +0100

    connected to mongo cloud from cluster

--- index.js
+++ index.js
@@ -6,4 +6,20 @@ const port = 8000;
 
 app.get('/', (req, res) => { res.send("Connected to server")})
 
+app.get('/api/users', (req, res) => {
+
+    
+    const MongoClient = require('mongodb').MongoClient;
+    const uri = "mongodb+srv://Kelly:<password>@mycluster-dex7f.mongodb.net/test?retryWrites=true&w=majority";
+    const client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+    });
+
+
+
+} )
+
 app.listen(port, () => { console.log("Server is running on port " + port)});
